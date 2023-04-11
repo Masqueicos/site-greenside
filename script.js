61ba15d54713ede1d719fbcf74595f399f3e05bc
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2023-04-11 09:05:59 -0300

    commit js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var dia = 14;
+let login = "46275435810";
+let senha = "123456";
+
+console.log(login)
+console.log(senha)
+
+function conferirLogin () {
+    let loginInput = document.getElementById('emailCPF')
+    let senhaInput = document.getElementById('senha')
+
+    if (login == loginInput || senha == senhaInput) {
+        alert('Login certo')
+    } else {
+        alert('Login errado')
+    }
+}
