50db25b3e0e09133db78d0f527dc87162ca6b62e
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2023-04-11 09:43:15 -0300

    commit js ;)

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let senha = "123456";
 console.log(login)
 console.log(senha)
 
-function conferirLogin () {
+function conferirLogin() {
     let loginInput = document.getElementById('emailCPF').value;
     let senhaInput = document.getElementById('senha').value;
 
     if (login === loginInput && senha === senhaInput) {
-        alert('Login certo')
+        alert('Login correto')
     } else {
         alert('Login ou senha incorretos')
     }
