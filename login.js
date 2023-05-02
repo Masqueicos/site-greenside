4e301f7d191f98ff34655d343e52b12172bd55a3
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2023-05-02 11:02:18 -0300

    commit js

--- login.js
+++ login.js
@@ -1,28 +1,4 @@
 /*
-var user;
-var senha;
-var recorrente;
-
-recorrente = true;
-
-function conferirLoginDo() {
-    user = window.prompt('Digite seu usuário:');
-    senha = window.prompt('Digite sua senha:');
-
-    do {
-        if (user == "Adri") {
-            if (senha == 123) {
-                window.alert("Você será redirecionado ao seu perfil")
-                recorrente = false
-            } else {
-                window.alert("Usuário ou senha inválido")
-            }
-        } else {
-            window.alert("Usuário ou senha inválido")
-        }
-    } while (recorrente == true);
-}
-
 function conferirLoginWhile() {
     user = window.prompt('Digite seu usuário:');
     senha = window.prompt('Digite sua senha:');
@@ -30,24 +6,6 @@ function conferirLoginWhile() {
     while (user == "Adri" && senha == 123) {
         window.alert("Você será redirecionado ao seu perfil")
     }
-}
-
-////////////////////////////////////////////////////////////////
-
-var loginObjeto = {
-    usuario: ['user1', 'user2', 'user3', 'user4'],
-    senha: ['12345', '67890', '56663', '77879'],
-}
-
-////////////////////////////////////////////////////////////////
-
-var usuarioeSenha = [
-    {"usuario": 'Alex', "senha": '1234'}
-]
-
-let dados = {
-    usuario: ['Alex', 'Jonas', 'Leon', 'Ashley'],
-    senha: ['dghjk', 'fhjgk', 'gdhgku', 'fyhuij'],
 }
 
 for (let i = 0; i < dados[usuario].length; i++) {
@@ -65,10 +23,10 @@ for (let usuariosIndex = 0; usuariosIndex < array.length; usuariosIndex++) {
 */
 
 var usuariosDB = [
-    { usuario: "leon", senha: "1234" },
-    { usuario: "adriano imperador", senha: "4321" },
-    { usuario: "tonio rodrigues", senha: "5678" },
-    { usuario: "wesker", senha: "8675" },
+    { usuario: "32493282000", senha: "1234" },
+    { usuario: "27948535039", senha: "4321" },
+    { usuario: "30632214066", senha: "5678" },
+    { usuario: "29715601049", senha: "8675" },
 ];
 
 var usuarioValidar;
@@ -76,22 +34,22 @@ var senhaValidar;
 var validacaoCompleta = false;
 
 function validarLogin(event) {
+
     event.preventDefault();
 
     usuarioValidar = document.getElementById("usuarioInput").value;
     senhaValidar = document.getElementById("senhaInput").value;
 
     let user = usuariosDB.find(function (user) {
-        return user.usuario === usuarioValidar;
-    })
-
+        return user.usuario == usuarioValidar;
+    });
     let password = usuariosDB.find(function (password) {
-        return password.senha === senhaValidar;
-    })
+        return password.senha == senhaValidar;
+    });
 
     do {
-        if (user === usuarioValidar) {
-            if (senha === senhaValidar) {
+        if (user == usuarioValidar) {
+            if (senha == senhaValidar) {
                 validacaoCompleta = true;
                 console.log("Login correto");
             } else {
@@ -100,20 +58,5 @@ function validarLogin(event) {
         } else {
             console.log("Login errado")
         }
-    } while (validacaoCompleta = false); 
+    } while (validacaoCompleta == false); 
 }
-
-/*
-do {
-    if (user == "Adri") {
-        if (senha == 123) {
-            window.alert("Você será redirecionado ao seu perfil")
-            recorrente = false
-        } else {
-            window.alert("Usuário ou senha inválido")
-        }
-    } else {
-        window.alert("Usuário ou senha inválido")
-    }
-} while (recorrente == true);
-*/
